234fabab00705eabbfb1858481d78dfd081fc14e
miguel <miguel.garay@hotmail.se>
2024-04-02 11:41:57 +0200

    lagt in komponenter i App.jsx

---
+++ src/Components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+        <main className='main'>
+            <h2>My Main Content</h2>
+            <p>här visas produkterna</p>
+        </main>
+    </>
+  )
+}
+
+export default Main
