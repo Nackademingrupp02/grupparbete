ed23659540ab8fb89bbed93cc56a181d726c86d0
miguel <miguel.garay@hotmail.se>
2024-04-02 17:50:52 +0200

    basically ingen ändring

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from './Components/Header';
 import Main from './Components/Main';
-import Footer from './Components/Footer '
+import Footer from './Components/Footer ';
 
 function App() {
   return (
