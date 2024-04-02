d29209e3436dacf988430dc761465ad8db40f6f9
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-04-02 11:38:12 +0200

    added more strict code to make more user friendly

--- src/models/ProductSchema.js
+++ src/models/ProductSchema.js
@@ -4,9 +4,9 @@ import  mongoose from "mongoose"
 
 
 const ProductsSchema = new mongoose.Schema({
-    name: { type: String, },
-    price: { type: Number },
-    category: { type: String },
+    name: { type: String, minlength:2, maxLength:20 , required:true},
+    price: { type: Number, min: 1, max:100000, required:true},
+    category: { type: String, required:true, minLength: 2, maxLength: 20},
 })
 const Product = mongoose.model("Product", ProductsSchema)
 export default  Product
