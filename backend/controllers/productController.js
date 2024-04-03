7fbfff0cd5f2b4f71d8e060de029caf5c4345ac5
jko.ridder <jko.ridder@gmail.com>
2024-04-03 14:24:45 +0200

    added function to add products to db

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Products = require('../models/ProductSchema.js');
+const jsonData = require('../../Testdata_Sprint_1.json');
 
 async function getAllProducts() {
     try {
@@ -10,4 +11,44 @@ async function getAllProducts() {
     }
 }
 
-export { getAllProducts };
+async function addProduct(req, res) {
+    try {
+        const { name, price, category } = req.body;
+
+        const product = new Products({
+            name,
+            price,
+            category
+        });
+        
+        await product.save();
+
+        res.status(201).json({ success: true, message: 'Product added successfully' });
+    } catch (error) {
+      console.error('Error adding product:', error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+}
+
+
+//Funktion för att lägga till data direkt från JSON. 
+async function addProductFromJSONData() {
+    try {
+        for (const productData of jsonData) {
+            const { name, price, category } = productData;
+
+            const product = new Products({
+                name:name,
+                price:price,
+                category:category
+            });
+
+            await product.save();
+        }
+        console.log('Products added successfully');
+    } catch(error) {
+        console.error('Error adding product:', error)
+    }
+}
+
+module.exports = { getAllProducts, addProduct, addProductFromJSONData };
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const connectToMongoose = require("./config/mongoose.js");
+const { addProductFromJSONData } = require("./controllers/productController.js");
 const app = require("./express.js");
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log("Server is Running on port ", port);
