feca16761feba672f08674daa818f02d1afe6d1d
miguel <miguel.garay@hotmail.se>
2024-04-03 15:54:52 +0200

    frontend ändringar

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -1,39 +1,52 @@
 import React from 'react'
 import Product from './Product.jsx'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Main = () => {
 
 
-  let dummyProducts = [
-    "product1",
-    "product2",
-    "product3",
-    "product4",
-    "product5",
-    "product6",
-    "product7",
-    "product8",
-    "product9",
-    "product10",
-    "product11",
-    "product12",
-    "product13",
-    "product14",
-    "product15",
-    "product16",
-    "product17",
-    "product18",
-    "product19",
-    "product20",
-  ]
+  // let dummyProducts = [
+  //   "product1",
+  //   "product2",
+  //   "product3",
+  //   "product4",
+  //   "product5",
+  //   "product6",
+  //   "product7",
+  //   "product8",
+  //   "product9",
+  //   "product10",
+  //   "product11",
+  //   "product12",
+  //   "product13",
+  //   "product14",
+  //   "product15",
+  //   "product16",
+  //   "product17",
+  //   "product18",
+  //   "product19",
+  //   "product20",
+  // ]
 
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("https://grupparbete.onrender.com/products/all");
+      const json = await response.json(); 
+      setProducts(json);
+    }
+    getData();
+  }, [])
+  
   return (
     <>
       <main className='main'>
         <h2>My Main Content</h2>
         <div>
           <ul>
-            {dummyProducts.map((product) => {
+            {products.map((product) => {
               
               return (
                 <>
--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Product = ({product}) => {
     return (
         <>
-            <p>{product}</p>
+            <p>Produkt: {product.name}</p>
+            <p>Kategori: {product.category}</p>
+            <p>Pris: {product.price}kr</p>
             <hr />
         </>
     )
