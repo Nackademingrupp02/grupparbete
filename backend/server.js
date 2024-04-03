0d715138666dc7e723fc04f2848aa12f99c037ea
jko.ridder <jko.ridder@gmail.com>
2024-04-03 14:49:05 +0200

    added route to show all products

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const connectToMongoose = require("./config/mongoose.js");
+const productRouter = require("./routes/productRoutes.js")
 // const { addProductFromJSONData } = require("./controllers/productController.js");
 const app = require("./express.js");
 
 const port = process.env.PORT || 8000;
+
+
+app.use('/products', productRouter);
 
 app.listen(port, () => {
   console.log("Server is Running on port ", port);
