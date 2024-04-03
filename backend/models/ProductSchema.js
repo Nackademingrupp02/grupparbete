9c2f4a6462cd926c883902fc85d5d53d976ec616
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-04-03 11:47:35 +0200

    Added float to price, in database make it more clean.

--- backend/models/ProductSchema.js
+++ backend/models/ProductSchema.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const ProductsSchema = new mongoose.Schema({
-  name: { type: String, minlength: 2, maxLength: 20, required: true },
-  price: { type: Number, min: 1, max: 100000, required: true },
-  category: { type: String, required: true, minLength: 2, maxLength: 20 },
+    name: { type: String, minlength: 2, maxLength: 20, required: true },
+    price: { type: Number, float:true,min: 1, max: 100000, required: true },
+    category: { type: String, required: true, minLength: 2, maxLength: 20 },
 });
-const Products = mongoose.model("Products", ProductsSchema);
+const Product = mongoose.model("Products", ProductsSchema);
 
-module.exports = Products;
+module.exports = Product;
