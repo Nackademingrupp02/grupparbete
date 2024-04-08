27899d143a98ce70d28df8e4c8a4e22189999938
pinsamt <felix.vallejo99v@gmail.com>
2024-04-08 10:26:25 +0200

    Apihelper and update product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const productController = require('../controllers/productController.js')
+const express = require("express");
+const {
+  addProduct,
+  removeProduct,
+  updateProduct,
+} = require("../controllers/productController.js");
 
 const router = express.Router();
 
-router.get('/all', async (req, res) => {
-    try {
-        const products = await productController.getAllProducts();
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+router.get("/all", async (req, res) => {
+  try {
+    const products = await productController.getAllProducts();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-router.post('/add', productController.addProduct);
-
-router.delete('/delete/:id', productController.removeOneProduct);
-    
-
+router.post("/add", addProduct);
+router.delete("/delete/:id", removeProduct);
+router.put("/update/:id", updateProduct);
 
 module.exports = router;
