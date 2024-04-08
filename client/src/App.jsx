67713f53bc933e110269f6d1c8157edfeb9d6601
miguel <miguel.garay@hotmail.se>
2024-04-08 11:10:00 +0200

    ändrat mapp strukturen och håller på att addera admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import Header from './Components/Header.jsx';
-import Main from './Components/Main.jsx'
-import Footer from './Components/Footer.jsx';
 import './App.css'
+import HomePage from './Pages/Homepage/HomePage'
+import AdminPage from './Pages/AdminPage/AdminPage'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Routes>
+        <Route path={'/'} element={<HomePage />}/>
+        <Route path={'/adminpage'} element={<AdminPage />} />
+      </Routes>
     </>
   )
 }
---
+++ client/src/Pages/Homepage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header.jsx'
+import Main from './Main.jsx'
+import Footer from './Footer.jsx'
+
+const HomePage = () => {
+  return (
+    <>
+        <Header />
+        <Main />
+        <Footer />
+    </>
+  )
+}
+
+export default HomePage
