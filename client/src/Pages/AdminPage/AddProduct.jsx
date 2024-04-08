39408ed4d9b6d79b8fb4d1c679121a9e8ed33047
miguel <miguel.garay@hotmail.se>
2024-04-08 16:03:45 +0200

    Adderat modal och lite css på den

--- client/src/Pages/AdminPage/AddProduct.jsx
+++ client/src/Pages/AdminPage/AddProduct.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import Modal from './Modal.jsx'
 
 const AddProduct = () => {
 
@@ -10,22 +11,7 @@ const AddProduct = () => {
       <button onClick={() => {
         setAddProduct(!addProduct)
       }}>Add product</button>
-      {addProduct && ( <div className="modal">
-        <div className='overlay'>
-          <div className='modal-content'>
-            <h3>Add product</h3>
-            <label htmlFor="">Name</label>
-            <input type="text" name="" id="name" />
-            <label htmlFor="">Category</label>
-            <input type="text" name="" id="category" />
-            <label htmlFor="">Price</label>
-            <input type="text" name="" id="price" />
-            <button onClick={() => {
-              setAddProduct(!addProduct)
-            }}>Close</button>
-          </div>
-        </div>
-      </div>)}
+      {addProduct && <Modal {...{setAddProduct}}/>}
     </>
   )
 }
--- client/src/Pages/AdminPage/Modal.jsx
+++ client/src/Pages/AdminPage/Modal.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const Modal = () => {
+const Modal = ({setAddProduct}) => {
   return (
     <>
-        
+        <div className="modal">
+        <div className='overlay'>
+          <div className='modal-content'>
+            <h3>Add product</h3>
+            <div className='addProductInputs'>
+            <label htmlFor="">Name</label>
+            <input type="text" name="" id="name" />
+            <label htmlFor="">Category</label>
+            <input type="text" name="" id="category" />
+            <label htmlFor="">Price</label>
+            <input type="text" name="" id="price" />
+            </div>
+            <button onClick={() => {
+              setAddProduct(false)
+            }}>Close</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
