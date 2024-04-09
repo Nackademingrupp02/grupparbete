2775776f1d9c3d145998fa146a8005fba729ac46
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-04-09 22:20:22 +0200

    added unfinished search product

--- client/src/Pages/Homepage/Main.jsx
+++ client/src/Pages/Homepage/Main.jsx
@@ -1,35 +1,36 @@
-import React from 'react';
-import Product from '../../Components/Product.jsx';
-import ErrorMessage from '../../Components/NoProductError.jsx';
-
+import React from "react";
+import Product from "../../Components/Product.jsx";
+import ErrorMessage from "../../Components/NoProductError.jsx";
+import { searchProducts } from "../../../../backend/controllers/productController.js";
 const Main = ({ products }) => {
   if (!Array.isArray(products)) {
     return (
-      <main className='main'>
-        <ErrorMessage message="Failed to fetch products."/>
+      <main className="main">
+        <searchProducts />
+        <ErrorMessage message="Failed to fetch products." />
       </main>
     );
   } else if (products.length === 0) {
-    return ( 
-      <main className='main'>
-        <ErrorMessage message={"No products found."}/>
+    return (
+      <main className="main">
+        <ErrorMessage message={"No products found."} />
       </main>
     );
   } else {
-  return (
-    <main className='main'>
-      <div>
-        <ul>
-          {products.map((product, index) => (
-            <div key={index}>
-              <Product {...{ product }} />
-            </div>
-          ))}
-        </ul>
-      </div>
-    </main>
-  );
-}
+    return (
+      <main className="main">
+        <div>
+          <ul>
+            {products.map((product, index) => (
+              <div key={index}>
+                <Product {...{ product }} />
+              </div>
+            ))}
+          </ul>
+        </div>
+      </main>
+    );
+  }
 };
 
 export default Main;
