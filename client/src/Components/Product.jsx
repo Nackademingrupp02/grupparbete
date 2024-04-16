b4d7f840580b3fa6795178fef260c4e2b23f48bb
pinsamt <felix.vallejo99v@gmail.com>
2024-04-16 11:01:12 +0200

    Changed Alot

--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -1,24 +1,103 @@
 import React from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import { useState } from "react";
+import {
+  Card,
+  ListGroup,
+  Container,
+  Row,
+  Col,
+  Button,
+  Modal,
+  ListGroupItem,
+} from "react-bootstrap";
 
 const Product = ({ product }) => {
+  const [modalShow, setModalShow] = useState(false);
+
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header>
+          <Row>
+            <Col>
+              <img
+                src={product.picture}
+                style={{ width: "20rem", height: "30rem", objectFit: "cover" }}
+              />
+            </Col>
+            <Col>
+              <ListGroup style={{ width: "20rem" }}>
+                <ListGroup.Item>
+                  <h2>{product.name}</h2>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <h2>{product.price} kr</h2>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <h3>{product.comparePrice} kr</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <h3>{product.packageSize}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <h4>{product.brand}</h4>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Innehållsförteckning</h4>
+          {product.ingredients}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  const handleModal = () => {
+    setModalShow(true);
+  };
   return (
     <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={product.picture} />
-        <Card.Body>
-          <Card.Title>{product.name}</Card.Title>
-          <Card.Text>{product.description}</Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item>{product.price} kr</ListGroup.Item>
-          <ListGroup.Item>{product.comparePrice}</ListGroup.Item>
-          <ListGroup.Item>{product.brand}</ListGroup.Item>
-        </ListGroup>
-        <Card.Body>
-          <Card.Link href="#">Köp</Card.Link>
-        </Card.Body>
-      </Card>
+      <Container>
+        <Row>
+          <Card style={{ width: "18rem" }} onClick={handleModal}>
+            <Card.Img
+              variant="top"
+              src="https://cdn.starwebserver.se/shops/testbutik1503/files/ma1010804-bulgur-500g-ekologiskt-urtekram-matgryn-kolonialvaror.png "
+              style={{ width: "20rem", height: "30rem", objectFit: "cover" }}
+            />
+            <Card.Body>
+              <Card.Title>{product.name}</Card.Title>
+              <Card.Text>{product.description}</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>{product.price} kr</ListGroup.Item>
+              <ListGroup.Item>{product.comparePrice}</ListGroup.Item>
+              <ListGroup.Item>
+                {product.brand} | {product.packageSize}
+              </ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Button variant="danger" className="px-4" size="lg">
+                Köp
+              </Button>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </>
   );
 };
--- client/src/Pages/Homepage/Header.jsx
+++ client/src/Pages/Homepage/Header.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
       <header>
-        <nav className='navHeader'>
-        <Link to='/'><h1 className='headerLinks'>Hakim livs</h1></Link>
-          <Link to='/adminpage'>Admin page</Link>
+        <nav className="navHeader" style={{ minWidth: "100vh" }}>
+          <Link to="/">
+            <h1 className="headerLinks">Hakim livs</h1>
+          </Link>
+          <Link to="/adminpage">Admin page</Link>
         </nav>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
