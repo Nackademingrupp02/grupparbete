059ad57584d973e23491ea6daa8a4eecf3bb5fe1
jko.ridder <jko.ridder@gmail.com>
2024-04-17 23:03:42 +0200

    added ways to edit order status.

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,6 +4,7 @@ const {
   removeOrder,
   getAllOrders,
   getOrder,
+  updateOrderStatus,
 } = require("../controllers/orderController");
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get("/all", getAllOrders);
 router.get("/order/:id", getOrder);
 router.post("/add", addOrder);
 router.delete("/delete/:id", removeOrder);
+router.put("/update/:id/status", updateOrderStatus);
 
 module.exports = router;
